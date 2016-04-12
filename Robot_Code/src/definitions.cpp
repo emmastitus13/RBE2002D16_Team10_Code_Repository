f3980bc2f214338a6bd82804546ba9fcb5f6f1d1
Ben Titus <ben.titus13@gmail.com>
2016-04-11 23:19:37 -0400

    Initial driving code
    Getting code to make the drive motors function properly

--- Robot_Code/src/definitions.cpp
+++ Robot_Code/src/definitions.cpp
@@ -1,10 +1,14 @@
+//defining states
 #define STOP 0
 #define FIND_CANDLE 1
 #define EXTINGUISH_FIRE 2
 #define RETURN_HOME 3
 
+//Ultrasonic sensor Maximum distance
+#define MAX_DISTANCE 200
 
 //Analog Pins
+#define FLAME_SENSE A0
 //1
 //2
 //3
@@ -24,35 +28,36 @@
 
 
 //Pin declarations
-//1
-//2
-//3
-//4
-//5
-#define FAN_PIN 6
-#define EXTINGUISH_SERVO_PIN 7
-#define RIGHT_MOTOR_PIN2 8
-#define RIGHT_MOTOR_PIN1 9
-#define LEFT_MOTOR_PIN2 10
-#define LEFT_MOTOR_PIN1 11
-//12
-//13
-//14
-//15
-//16
-//17
-//18
-//19
-//20
-//21
+//0 RX0
+//1 TX0
+#define L_ENCODER_PIN 2//PWM INT0
+#define R_ENCODER_PIN 3//3 PWM INT1
+//4 PWM
+//5 PWM
+#define FAN_PIN 6              // PWM
+#define EXTINGUISH_SERVO_PIN 7 // PWM
+#define RIGHT_MOTOR_PIN2 8     // PWM
+#define RIGHT_MOTOR_PIN1 9     // PWM
+#define LEFT_MOTOR_PIN2 10     // PWM
+#define LEFT_MOTOR_PIN1 11     // PWM
+//12 PWM
+//13 PWM LED
+//14 TX3
+//15 RX3
+//16 TX2
+//17 RX2
+//18 INT5 TX1
+//19 INT4 RX1
+//20 INT3 SDA
+//21 INT2 SCL
 //22
 //23
 //24
 //25
-//26
-//27
-//28
-//29
+#define LEFT_US_EP 26
+#define LEFT_US_TP 27
+#define FORWARD_US_TP 28
+#define FORWARD_US_EP 29
 //30
 //31
 //32
@@ -67,11 +72,13 @@
 //41
 //42
 //43
-//44
-//45
-//46
+//44 PWM
+//45 PWM
+//46 PWM
 //47
 //48
 //49
-//50
-//51
+//50 MISO
+//51 MOSI
+#define RIGHT_US_TP 52//SCK
+#define RIGHT_US_EP 53//SS
--- Robot_Code/src/main.cpp
+++ Robot_Code/src/main.cpp
@@ -1,20 +1,69 @@
 #include <Arduino.h>
 #include "driveFunctions.cpp"
-#include "H-BridgeMotorDriver.h"
-#include ""
+#include "definitions.cpp"
+#include "fireExtinguisher.cpp"
+#include "hBridgeMotorDriver.h"
+#include "NewPing.h"
+
+bool test = true;
+
+void lEncoderISR(void);
+void rEncoderISR(void);
+
+
+NewPing leftUS(LEFT_US_TP, LEFT_US_EP, MAX_DISTANCE);
+NewPing rightUS(RIGHT_US_TP, RIGHT_US_EP, MAX_DISTANCE);
+NewPing forwardUS(FORWARD_US_TP, FORWARD_US_EP, MAX_DISTANCE);
 
 volatile unsigned char botState = STOP;
+volatile unsigned long lEncode = 0;
+volatile unsigned long rEncode = 0;
+unsigned long currentL = 0, currentR = 0;
 
-Type1_Motor leftMotor, rightMotor, fans;
-
+Type2_Motor leftMotor(LEFT_MOTOR_PIN1, LEFT_MOTOR_PIN2);
+Type2_Motor rightMotor(RIGHT_MOTOR_PIN1, RIGHT_MOTOR_PIN2);
 
 void setup() {
+    Serial.begin(115200);
+
+    pinMode(L_ENCODER_PIN, INPUT_PULLUP);
+    pinMode(R_ENCODER_PIN, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(L_ENCODER_PIN), lEncoderISR, FALLING);
+    attachInterrupt(digitalPinToInterrupt(R_ENCODER_PIN), rEncoderISR, FALLING);
 
 }
 
 void loop() {
-    switch (botState) {
-        case STOP:
+    if (test) {
+        delay(50);
+        int uS = leftUS.ping();
+        Serial.print("Ping: ");
+        Serial.print(uS / US_ROUNDTRIP_CM);
+        Serial.println("cm");
+        /*
+        leftMotor.driveBackward(255);
+        rightMotor.driveForward(255);
+        Serial.print("Left Encoder: ");
+        Serial.print((lEncode - currentL) / 500);
+        Serial.print(" Right Encoder: ");
+        Serial.println((rEncode - currentR) / 500);
+        delay(500);
+        currentL = lEncode;
+        currentR = rEncode;*/
+    } else {
+        switch (botState) {
+            case STOP:
+                break;
 
+        }
     }
+}
+
+
+void lEncoderISR(void) {
+    lEncode++;
+}
+void rEncoderISR(void) {
+    rEncode++;
 }
